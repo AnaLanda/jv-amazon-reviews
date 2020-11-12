5e2aca61f1e780035d9b836c308137fa84e103b2
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-11-12 09:35:37 +0200

    removed unused test file

--- src/test/java/com/amazon/reviews/service/impl/CsvParsingServiceImplTest.java
+++ src/test/java/com/amazon/reviews/service/impl/CsvParsingServiceImplTest.java
@@ -2,18 +2,19 @@ package com.amazon.reviews.service.impl;
 
 import com.amazon.reviews.exception.FileProcessingException;
 import com.amazon.reviews.model.dto.ReviewDto;
+import com.amazon.reviews.service.FileReaderService;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CsvParsingServiceImplTest {
     private final CsvToReviewDtoParsingServiceImpl service = new CsvToReviewDtoParsingServiceImpl();
+    private final FileReaderService reader = new FileReaderServiceImpl();
     private final String line = "1, B001E4KFG0, A3SGXH7AUHU8GW, delmartian, 1, 1, 5, 1303862400, "
             + "Good Quality Dog Food, I have bought several of the Vitality "
             + "canned dog food products, and I have found them all to be of good quality. "
             + "The product looks more like a stew than a processed meat and it smells better. "
             + "My Labrador is finicky, and she appreciates this product better than most.";
     private final String badLine = "1, B001E4KFG0";
-    private static final String TEST_FILE = "src/test/resources/test_review.csv";
 
     @Test
     public void parse_Ok() {
